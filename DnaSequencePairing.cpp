1dc760c8ad50053c7eef0a5bb78f693be72d3f6f
Mahmood Qawasmi <m7mood.nael.1996@gmail.com>
2020-11-11 13:17:00 +0200

    Revert "will revert"
    This reverts commit 8fed71cb089a590e17197375d40f247b87c3e677.

--- DnaSequencePairing.cpp
+++ DnaSequencePairing.cpp
@@ -29,9 +29,10 @@ DnaSequence DnaSequencePairing::Pairing(DnaSequence &dnaSequence) {
     return DnaSequence(newDna);
 }
 
-void DnaSequencePairing::reverseStr(string &str) {
+
+void reverseStr(string& str)
+{
     int n = str.length();
     for (int i = 0; i < n / 2; i++)
         swap(str[i], str[n - i - 1]);
 }
-
